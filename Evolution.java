fe0210e1c900d0f4a3fd412f9ae9f7ab78397bdc
Braden-G <bradengarrett3@example.com>
2022-10-28 16:30:55 -0600

    Outline with future plans commented

--- Evolution.java
+++ Evolution.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Evolution{
-    private int population = 0;
+    private int popRabbits = 0;
+    public int RPH; //Random Place Holder
     public class world{
         
 
@@ -14,6 +15,10 @@ public class Evolution{
     public class creature{
         public void reproduce(int population){
             //based on rng give chances for reproduction
+            RPH = RandomInRange(1, 100);
+            if(RPH > 10 && RPH <= 1){
+                //1 in in 10 chanve
+            }
         }
         public void die(int population){
             //based on rng give chances for decrease in population
